878bb84ecaff1b1d751f65389f940f6f1f4f38f7
Lu Zhu <zhu.lu@hotmail.com>
2023-10-11 16:24:27 -0400

    add visualization utils

--- polaris/curation/utils.py
+++ polaris/curation/utils.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from typing import List, TypeVar, Union
 
+from loguru import logger
 import numpy as np
 from pydantic import BaseModel, Field
 from scipy import stats
@@ -17,8 +18,18 @@ from sklearn.svm import OneClassSVM
 from sklearn.utils import check_array
 from sklearn.utils.validation import check_is_fitted
 
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy import stats
+import datamol as dm
+from IPython.display import display
+from ._chemistry_curator import NUM_UNDEF_STEREO_CENTER, NUM_DEF_STEREO_CENTER
+
 PandasDataFrame = TypeVar("pandas.core.frame.DataFrame")
 NumpyNDArray = TypeVar("numpy.ndarray")
+
+NUM_UNDEF_STEREO_CENTER = "num_undefined_stereo_center"
+NUM_DEF_STEREO_CENTER = "num_defined_stereo_center"
 
 
 class LabelOrder(Enum):
@@ -246,3 +257,98 @@ def outlier_detection(X: np.ndarray, method="zscore", **kwargs) -> np.ndarray:
     pred = detector.fit_predict(X)
     outlier_index = np.argwhere(pred == -1)
     return outlier_index
+
+
+def visulize_distribution(data_cols: List[str], dataset: PandasDataFrame):
+    """Visualize the distribution of the data and highlight the potential outliers.
+
+    Args:
+        data_cols: Column names which the `OUTLIER` was previously detected.
+        dataset: Dataset Dataframe which inludes columns of `OUTLIER` flags.
+
+    Returns:
+        figs: List of distribution plots.
+    """
+    figs = []
+    for col in data_cols:
+        outlier_col = f"OUTLIER_{col}"
+        if outlier_col not in dataset.columns:
+            raise ValueError(
+                f"{outlier_col} is not found in the given dataset.\
+                             Please run <polaris.curation.utils.outlier_detection> first."
+            )
+        fig, axes = plt.subplots(ncols=2, nrows=1, figsize=(16, 4))
+        to_plot = dataset.sort_values(by=col).reset_index()
+        to_plot["order"] = range(to_plot.shape[0])
+        sns.scatterplot(
+            to_plot,
+            x="order",
+            y=col,
+            hue=outlier_col,
+            palette={True: "red", False: "navy"},
+            ax=axes[0],
+        )
+        stats.probplot(to_plot[col].values, dist="norm", plot=axes[1])
+        figs.append(fig)
+    plt.close()
+    return figs
+
+
+def verify_stereoisomers(data_cols: List[str], dataset: PandasDataFrame):
+    """Visual verification the stereoisomers from the original dataset
+
+    Args:
+        data_cols: Column names which the activity cliff was previously detected.
+        dataset: Dataframe which includes stereo chemistry information of the molcules.
+
+    """
+    for col in data_cols:
+        logger.info(f"Verify the stereo ismomers for readout `{col}`")
+        cliff_col = f"CLASS_{col}_stereo_cliff"
+        if cliff_col not in data_cols:
+            raise ValueError(
+                "The stereo chemistry information is unavailable. \
+                             Please run the <polaris.curation.run_chemistry_curation> first."
+            )
+        cliff = dataset[cliff_col].notna()
+        if cliff.sum() > 0:
+            to_plot = dataset.loc[
+                cliff, ["smiles", NUM_UNDEF_STEREO_CENTER, NUM_DEF_STEREO_CENTER, col, cliff_col]
+            ]
+            display(to_plot)  # display the rows of dataframe
+            display(dm.to_image([dm.to_mol(s) for s in to_plot.smiles]))  # display the molecules
+        else:
+            logger.info("No activity cliffs found in stereosimoers.")
+
+
+def check_undefined_stereocenters(dataset: PandasDataFrame):
+    """Visual check the molecules with undefined stereo centers
+
+    Args:
+        dataset: Dataframe which includes stereo chemistry information of the molcules.
+
+    Returns:
+        fig: Image figure of the molecules with stereo center annotations.
+
+    See Also:
+        <polaris.curation.run_chemistry_curation>
+
+    """
+
+    if NUM_UNDEF_STEREO_CENTER not in dataset or NUM_DEF_STEREO_CENTER not in dataset:
+        raise ValueError(
+            "The stereo information of the molecules are unavailable.\
+                         Please run the <polaris.curation.run_chemistry_curation> first."
+        )
+
+    to_plot = dataset[dataset[[NUM_UNDEF_STEREO_CENTER, NUM_DEF_STEREO_CENTER]].notna().any(axis=1)]
+    legends = (
+        to_plot[[NUM_UNDEF_STEREO_CENTER, NUM_DEF_STEREO_CENTER]]
+        .apply(
+            lambda x: f"num_undefined_stereo_center:{x[NUM_UNDEF_STEREO_CENTER]}\n num_defined_stereo_center{x[NUM_DEF_STEREO_CENTER]}",
+            axis=1,
+        )
+        .tolist()
+    )
+    fig = dm.to_image(to_plot.mol.tolist(), legends=legends)
+    return fig
