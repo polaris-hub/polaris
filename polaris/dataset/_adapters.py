bed9b53b260aa05d178d76d6c47b5ec1e389edda
Andrew Quirke <andrewquirke99@gmail.com>
2024-04-02 17:02:40 -0400

    Updating Adapter enum with unique decorator and auto generated values

--- polaris/dataset/_adapters.py
+++ polaris/dataset/_adapters.py
@@ -1,10 +1,10 @@
-from enum import Enum
+from enum import Enum, auto, unique
 import datamol as dm
 
 # Map of conversion operations which can be applied to dataset columns
 conversion_map = {"SMILES_TO_MOL": dm.to_mol, "BYTES_TO_MOL": dm.Mol}
 
-
+@unique
 class Adapter(Enum):
     """
     Adapters are predefined callables that change the format of the data.
@@ -15,8 +15,8 @@ class Adapter(Enum):
         BYTES_TO_MOL: Convert a RDKit binary string to a RDKit molecule.
     """
 
-    SMILES_TO_MOL = "SMILES_TO_MOL"
-    BYTES_TO_MOL = "BYTES_TO_MOL"
+    SMILES_TO_MOL = auto()
+    BYTES_TO_MOL = auto()
 
     def __call__(self, data):
         if isinstance(data, tuple):
