d42eeecf55d11fd572e46cae5da9dba51ff9836d
Lu Zhu <zhu.lu@hotmail.com>
2024-04-25 16:36:47 -0400

    update docs

--- docs/api/evaluation.md
+++ docs/api/evaluation.md
@@ -5,6 +5,8 @@
 
 ::: polaris.evaluate.MetricInfo
 
+::: polaris.evaluate._metric.absolute_average_fold_error
+
 ---
 
 ::: polaris.evaluate.Metric
--- polaris/utils/types.py
+++ polaris/utils/types.py
@@ -71,7 +71,7 @@ A validated URL that will be turned into a string.
 This is useful for interactions with httpx and authlib, who have their own URL types.
 """
 
-DirectionType: TypeAlias = Literal["min", "max"]
+DirectionType: TypeAlias = float | Literal["min", "max"]
 """
 The direction of any variable to be sorted.
 This can be used to sort the metric score, indicate the optmization direction of endpoint.
