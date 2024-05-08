4dba55784488bb2cf2daf19c16727519f8bcb1cc
cwognum <caswognum@outlook.com>
2024-05-08 15:37:33 -0400

    First implementation of the zarr checksum
    Mostly uses the code from the zarr-checksum library

--- polaris/dataset/zarr/__init__.py
+++ polaris/dataset/zarr/__init__.py
@@ -1,3 +1,4 @@
+from ._checksum import compute_zarr_checksum
 from ._memmap import MemoryMappedDirectoryStore
 
-__all__ = ["MemoryMappedDirectoryStore"]
+__all__ = ["MemoryMappedDirectoryStore", "compute_zarr_checksum"]
