c0224971b17108df3c3978343715797f43221fdb
Kira McLean <email@kiramclean.com>
2024-05-28 23:02:09 -0300

    include evaluate_benchmark in package

--- polaris/evaluate/__init__.py
+++ polaris/evaluate/__init__.py
@@ -1,4 +1,9 @@
 from polaris.evaluate._metric import Metric, MetricInfo
 from polaris.evaluate._results import BenchmarkResults, ResultsType
+from polaris.evaluate.utils import evaluate_benchmark
 
-__all__ = ["Metric", "MetricInfo", "BenchmarkResults", "ResultsType"]
+__all__ = ["Metric",
+           "MetricInfo",
+           "BenchmarkResults",
+           "ResultsType",
+           "evaluate_benchmark"]
