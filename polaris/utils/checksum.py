3a89b457576d22b6ef3731842e155615c208f735
Andrew Quirke <andrewquirke99@gmail.com>
2024-08-27 17:36:00 -0600

    Updating argument types for md5 util

--- polaris/utils/checksum.py
+++ polaris/utils/checksum.py
@@ -1,7 +1,7 @@
 from hashlib import md5
 
 
-def calculate_file_md5(file_path):
+def calculate_file_md5(file_path: str):
     """Calculates the md5 hash for a file at a given path"""
 
     md5_hash = md5()
