f64bcdcfce9d047b3d00f88c7b7cd7cecdc30766
Cas Wognum <caswognum@outlook.com>
2025-02-01 10:10:16 -0500

    Fix: Always pass bytes in the RDKitMolCodec (#261)

--- tests/test_codecs.py
+++ tests/test_codecs.py
@@ -6,6 +6,13 @@ from polaris.dataset.zarr.codecs import RDKitMolCodec
 
 def test_rdkit_mol_codec():
     mol = dm.to_mol("C1=CC=CC=C1")
-    arr = zarr.array([mol, mol], chunks=(2,), dtype=object, object_codec=RDKitMolCodec())
+
+    arr = zarr.empty(shape=10, chunks=2, dtype=object, object_codec=RDKitMolCodec())
+
+    arr[0] = mol
+    arr[1] = mol
+    arr[2] = mol
+
     assert dm.same_mol(arr[0], mol)
     assert dm.same_mol(arr[1], mol)
+    assert dm.same_mol(arr[2], mol)
