92bb0bf7d821d8eecb9b737ce50f817cc93a93bc
roselyn! <48699438+roselynh100@users.noreply.github.com>
2025-04-08 11:29:44 -0400

    chore: Replace `Union` with `|` in type annotations (#281)
    * replace Union with |
    
    * Update polaris/evaluate/metrics/docking_metrics.py
    
    Co-authored-by: Honoré Hounwanou <mercuryseries@gmail.com>
    
    * Update polaris/evaluate/metrics/docking_metrics.py
    
    Co-authored-by: Honoré Hounwanou <mercuryseries@gmail.com>
    
    * Update polaris/utils/types.py
    
    Co-authored-by: Honoré Hounwanou <mercuryseries@gmail.com>
    
    * Update polaris/utils/types.py
    
    Co-authored-by: Honoré Hounwanou <mercuryseries@gmail.com>
    
    * remove other deprecated typing: Dict, List, Tuple
    
    ---------
    
    Co-authored-by: Honoré Hounwanou <mercuryseries@gmail.com>

--- polaris/evaluate/metrics/docking_metrics.py
+++ polaris/evaluate/metrics/docking_metrics.py
@@ -1,6 +1,5 @@
 # This script includes docking related evaluation metrics.
 
-from typing import Union, List
 
 import numpy as np
 from rdkit.Chem.rdMolAlign import CalcRMS
@@ -36,7 +35,7 @@ def _rmsd(mol_probe: dm.Mol, mol_ref: dm.Mol) -> float:
     )
 
 
-def rmsd_coverage(y_pred: Union[str, List[dm.Mol]], y_true: Union[str, list[dm.Mol]], max_rsmd: float = 2):
+def rmsd_coverage(y_pred: str | list[dm.Mol], y_true: str | list[dm.Mol], max_rsmd: float = 2):
     """
     Calculate the coverage of molecules with an RMSD less than a threshold (2 Å by default) compared to the reference molecule conformer.
 
