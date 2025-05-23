f4d6417482ee8955f3521d5ad1c49ac8e46a1fea
Jack Li <j279li@uwaterloo.ca>
2025-05-23 14:40:59 -0400

    update test

--- tests/test_hub_integration.py
+++ tests/test_hub_integration.py
@@ -8,19 +8,10 @@ from polaris.hub.settings import PolarisHubSettings
 settings = PolarisHubSettings()
 
 
-@pytest.mark.skipif(
-    settings.username is None or settings.password is None,
-    reason="This test case requires headless authentication to be set up",
-)
 def test_load_dataset_flow():
     dataset = po.load_dataset("polaris/hello-world")
     assert isinstance(dataset, BaseDataset)
 
-
-@pytest.mark.skipif(
-    settings.username is None or settings.password is None,
-    reason="This test case requires headless authentication to be set up",
-)
 def test_load_benchmark_flow():
     benchmark = po.load_benchmark("polaris/hello-world-benchmark")
     assert isinstance(benchmark, BenchmarkSpecification)
