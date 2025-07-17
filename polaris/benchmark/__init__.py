bd3c933f623c026f0d54863ee29556b82a0b420d
Jack Li <73399568+j279li@users.noreply.github.com>
2025-07-17 17:59:06 -0400

    Update polaris/benchmark/__init__.py
    Co-authored-by: Andrew Quirke <andrewquirke99@gmail.com>

--- polaris/benchmark/__init__.py
+++ polaris/benchmark/__init__.py
@@ -1,4 +1,15 @@
-from polaris.benchmark._base import BenchmarkV1Specification
+from polaris.benchmark._base import BenchmarkV1Specification, BenchmarkV1Specification as BenchmarkSpecification
+from polaris.benchmark._benchmark_v2 import BenchmarkV2Specification
+from polaris.benchmark._definitions import MultiTaskBenchmarkSpecification, SingleTaskBenchmarkSpecification
+
+__all__ = [
+    "BenchmarkSpecification",
+    "BenchmarkV1Specification",
+    "BenchmarkV2Specification",
+    "SingleTaskBenchmarkSpecification",
+    "MultiTaskBenchmarkSpecification",
+]
+
 from polaris.benchmark._benchmark_v2 import BenchmarkV2Specification
 from polaris.benchmark._definitions import MultiTaskBenchmarkSpecification, SingleTaskBenchmarkSpecification
 
