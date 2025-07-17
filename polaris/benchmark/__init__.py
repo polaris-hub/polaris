7c961be1946ba1edd6fe3d977af30ebea0b269a2
Jack Li <j279li@uwaterloo.ca>
2025-07-17 18:02:58 -0400

    fix duplicate imports + formatting

--- polaris/benchmark/__init__.py
+++ polaris/benchmark/__init__.py
@@ -1,19 +1,12 @@
-from polaris.benchmark._base import BenchmarkV1Specification, BenchmarkV1Specification as BenchmarkSpecification
+from polaris.benchmark._base import (
+    BenchmarkV1Specification,
+    BenchmarkV1Specification as BenchmarkSpecification,
+)
 from polaris.benchmark._benchmark_v2 import BenchmarkV2Specification
 from polaris.benchmark._definitions import MultiTaskBenchmarkSpecification, SingleTaskBenchmarkSpecification
 
 __all__ = [
     "BenchmarkSpecification",
-    "BenchmarkV1Specification",
-    "BenchmarkV2Specification",
-    "SingleTaskBenchmarkSpecification",
-    "MultiTaskBenchmarkSpecification",
-]
-
-from polaris.benchmark._benchmark_v2 import BenchmarkV2Specification
-from polaris.benchmark._definitions import MultiTaskBenchmarkSpecification, SingleTaskBenchmarkSpecification
-
-__all__ = [
     "BenchmarkV1Specification",
     "BenchmarkV2Specification",
     "SingleTaskBenchmarkSpecification",
